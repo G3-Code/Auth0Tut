a7c3f2ef735253d4e5bbe89d1586ab5e89532e24
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-08 19:57:04 -0500

    Set the state object and checked if the state engine was running fine

--- react-auth/src/App.js
+++ react-auth/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    console.log(" :::::PROPS IS :::::::" + this.props.name);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Welcome to React, {this.props.name}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
