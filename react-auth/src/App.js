3b0ddb87daf0bbc80c9ee53dd22dd6d439049c94
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-08 20:03:29 -0500

    Added Main component

--- react-auth/src/App.js
+++ react-auth/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Main from "./components/Main";
 
 class App extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>Welcome to React, {this.props.name}</p>
         </header>
+        <Main />
       </div>
     );
   }
---
+++ react-auth/src/components/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>This is a testing space for Main.</p>
+      </div>
+    );
+  }
+}
